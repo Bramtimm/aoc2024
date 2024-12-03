0ac705c8d391238ba275dbaf090fd8071575c292
bramtimm <bram.timm@gmail.com>
2024-12-04 00:01:58 +0100

    remove todo

--- rust/tests/advent_of_code1_integration.rs
+++ rust/tests/advent_of_code1_integration.rs
@@ -4,7 +4,6 @@ use advent_of_code_lib::advent_of_code1::advent_of_code1;
 #[test]
 fn test_advent_of_code1_integration(){
 
-    // TODO read txt file and split accordingly
     let mut file_path: &str = "../data/advent_of_code1/puzzle_input.txt";
     let contents = fs::read_to_string(&file_path).expect("Should have been able to read the file");
     
