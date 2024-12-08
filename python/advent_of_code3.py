37878e5ade15efcc4b2f9ac18a455a937e6e27a5
bramtimm <bram.timm@gmail.com>
2024-12-08 20:38:29 +0100

    add 3b to python

--- python/advent_of_code3.py
+++ python/advent_of_code3.py
@@ -8,6 +8,28 @@ def advent_of_code3a(input_str: str) -> int:
 
     return sum(x)
 
+def advent_of_code3b(input_str: str) -> int:
+
+    # mul before the first don't 
+    # print(re.split(r"(.*)don't\(\)", input_str)[:1])
+    # mul = [advent_of_code3a(before_first_dont)]
+
+    # split on do
+    input_str_splitted = re.split(pattern= r"do\(\)", string=input_str)
+
+    mul = []
+    for sub_str in input_str_splitted:
+
+        # take first element as we aren't interested in any sub_str after mul_str
+        mul_str = re.split(r"don't\(\)", sub_str)
+        mul.append(advent_of_code3a(mul_str[0]))
+
+    return sum(mul)
+
+
+
+
+    # do advent_of_code3a
 
 if __name__ ==  "__main__":
 
@@ -21,3 +43,12 @@ if __name__ ==  "__main__":
 
     puzzle_output_3a = advent_of_code3a(puzzle_input)
     print(f"the added multiplications of the input is: {puzzle_output_3a}!")
+
+    # 3B
+    test_input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    test_output_3b = advent_of_code3b(test_input)
+    print(f"the added multiplications of the input is: {test_output_3b}!")
+    assert test_output_3b == 48
+
+    puzzle_output_3b = advent_of_code3b(puzzle_input)
+    print(f"the added multiplications of the input is: {puzzle_output_3b}!")
