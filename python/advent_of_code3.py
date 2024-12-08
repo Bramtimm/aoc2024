7411deaf280436736e50d41705368cc89da3ca63
bramtimm <bram.timm@gmail.com>
2024-12-08 13:23:20 +0100

    add advent_of_code3a

---
+++ python/advent_of_code3.py
@@ -0,0 +1,23 @@
+import re
+
+def advent_of_code3a(input_str: str) -> int:
+
+    mul_pairs = re.findall(pattern = r"mul\((\d{1,3}),(\d{1,3})\)", string=input_str)
+
+    x = [int(pair[0]) * int(pair[1]) for pair in mul_pairs]
+
+    return sum(x)
+
+
+if __name__ ==  "__main__":
+
+    test_input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    test_output_3a = advent_of_code3a(test_input)
+    print(f"the added multiplications of the input is: {test_output_3a}!")
+    assert test_output_3a == 161
+
+    with open("../data/advent_of_code3/puzzle_input.txt", "r") as f:
+        puzzle_input = f.read()
+
+    puzzle_output_3a = advent_of_code3a(puzzle_input)
+    print(f"the added multiplications of the input is: {puzzle_output_3a}!")
