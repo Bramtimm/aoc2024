b3d9e91a64e4c80c5f9a508ec5ccd6e92f7f9452
bramtimm <bram.timm@gmail.com>
2024-12-11 21:01:57 +0100

    add 4b

--- python/advent_of_code4.py
+++ python/advent_of_code4.py
@@ -52,14 +52,49 @@ def advent_of_code4a(test_input: str | list, pattern = 'XMAS') -> int:
                 diag_count += len(re.findall(pattern = pattern, string = ''.join(sub_matrix.get_reverse_diagonal()))) 
                 diag_count += len(re.findall(pattern = reverse_pattern, string = ''.join(sub_matrix.get_diagonal()))) 
                 diag_count += len(re.findall(pattern = reverse_pattern, string = ''.join(sub_matrix.get_reverse_diagonal()))) 
-
-            
-               
-
               
     return row_count + col_count + diag_count
 
+def advent_of_code4b(test_input: str|list, cross_pattern = "MAS") -> int:
 
+    if isinstance(test_input, str):
+        puzzle_input = test_input.split()
+        puzzle_input = [list(line) for line in puzzle_input]
+ 
+    elif isinstance(test_input, list):
+        puzzle_input = [list(line[0]) for line in test_input]
+
+    nrows = len(puzzle_input)
+    ncols = len(puzzle_input[0])
+
+    puzzle_matrix = Matrix(nrow = nrows, ncol = ncols)
+    
+    for row in range(0, nrows):
+        for col in range(0, ncols):
+            puzzle_matrix[row, col] = puzzle_input[row][col]
+
+    pattern_len = len(list(cross_pattern))
+    pattern = rf'{cross_pattern}'
+    reverse_pattern = ''.join(reversed(list(pattern)))
+    cross_pattern = pattern + (f"|{reverse_pattern}")
+
+    cross_diag_count = 0 
+    for row in range(0, nrows):
+        for col in range(0, ncols):           
+            if row+pattern_len-1 in range(0,nrows) and col+pattern_len-1 in range(0,ncols):
+                sub_matrix = Matrix(pattern_len, pattern_len, fill='_')   
+                
+                for row_index, row2 in enumerate(range(row, row+pattern_len)):
+                    for col_index, col2 in enumerate(range(col, col+pattern_len)):
+                       
+                        sub_matrix[row_index, col_index] = puzzle_input[row2][col2]
+
+                        pattern_on_diagonals =len(re.findall(pattern = cross_pattern, string = ''.join(sub_matrix.get_diagonal()))) + len(re.findall(pattern = cross_pattern, string = ''.join(sub_matrix.get_reverse_diagonal())))
+
+                        if pattern_on_diagonals == 2:
+                             cross_diag_count += 1
+    
+    return cross_diag_count
 class Matrix:
 
     def __init__(self, nrow: int, ncol: int, fill: str = ""):
@@ -124,5 +159,11 @@ MXMXAXMASX"""
     
     puzzle_output = advent_of_code4a(puzzle_input)
     print(puzzle_output)
-         
+    
+    test_output = advent_of_code4b(test_input)
+    print(test_output)
+    assert test_output == 9
+
+    puzzle_output = advent_of_code4b(puzzle_input)
+    print(puzzle_output)
 
