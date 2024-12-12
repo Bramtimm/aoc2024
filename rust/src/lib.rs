3a38b94433accc5968ec6866543c5326bb4e81db
bramtimm <bram.timm@gmail.com>
2024-12-12 10:44:20 +0100

    add aoc 2a to rust

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,10 @@
 pub mod advent_of_code1;
+pub mod advent_of_code2;
 pub mod advent_of_code3;
 #[cfg(test)]
 mod tests {
     use crate::advent_of_code1::*;
+    use crate::advent_of_code2::*;
     use crate::advent_of_code3::*;
 
     #[test]
@@ -22,12 +24,26 @@ mod tests {
     }
 
     #[test]
+    fn test_advent_of_code2a() {
+        let mut vec_1: Vec<Vec<i32>> = vec![
+            vec![7, 6, 4, 2, 1],
+            vec![1, 2, 7, 8, 9],
+            vec![9, 7, 6, 2, 1],
+            vec![1, 3, 2, 4, 5],
+            vec![8, 6, 4, 4, 1],
+            vec![1, 3, 6, 7, 9],
+        ];
+        let result: i32 = advent_of_code2a(&mut vec_1);
+        assert_eq!(result, 2);
+    }
+
+    #[test]
     fn test_advent_of_code3a() {
-        let input_str: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        let input_str: &str =
+            "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
         let result: i32 = advent_of_code3a(input_str);
         assert_eq!(result, 161);
     }
-    
 }
 
 pub fn test() {
