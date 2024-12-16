333816c5d31f5818d47cad5e18139aa8396ee7ff
bramtimm <bram.timm@gmail.com>
2024-12-16 23:28:30 +0100

    add answer to aoc6

--- python/advent_of_code4.py
+++ python/advent_of_code4.py
@@ -1,5 +1,7 @@
 import re
 
+from utils import Matrix
+
 def advent_of_code4a(test_input: str | list, pattern = 'XMAS') -> int:
 
     if isinstance(test_input, str):
@@ -95,45 +97,7 @@ def advent_of_code4b(test_input: str|list, cross_pattern = "MAS") -> int:
                              cross_diag_count += 1
     
     return cross_diag_count
-class Matrix:
 
-    def __init__(self, nrow: int, ncol: int, fill: str = ""):
-        self.nrow = nrow
-        self.ncol = ncol
-        self.data = [[fill for _ in range(ncol)] for _ in range(nrow)]
-
-    def __getitem__(self, index):
-        row_index, col_index = index
-        return self.data[row_index][col_index]
-    
-    def __setitem__(self, index, value):
-        row_index, col_index = index
-        self.data[row_index][col_index] = value
-
-    def get_diagonal(self):
-
-        self.diagonal = []
-        for diag_index in range(0, self.nrow):
-            self.diagonal.append(self.data[diag_index][diag_index])
-
-        return self.diagonal
-
-    def get_reverse_diagonal(self):
-        
-        self.reverse_diagonal = []
-        
-        for diag_index in range(0, self.nrow):
-            self.reverse_diagonal.append(self.data[diag_index][self.ncol-diag_index-1])
-        return self.reverse_diagonal
-
-    def get_column(self, col_index):
-        
-        self.column = []
-        for i in range(0, self.nrow):
-            self.column.append(self.data[i][col_index])
-
-        return self.column
-  
 
 
 if __name__ == "__main__":
--- python/advent_of_code6.py
+++ python/advent_of_code6.py
@@ -1,23 +1,115 @@
-from advent_of_code4 import Matrix
+import copy
+
+from utils import Matrix
 
 class PositionMatrix(Matrix):
 
-    def __init__(self):
-        pass
+   
+    def __init__(self, nrow, ncol):
+        super().__init__(nrow, ncol)
 
-    # def get_position(self):
 
-    #     position == "^"
-    #     if position == "^":
-    #         direction = "up"
-    #     elif position == ">":
-    #         direction = "right"
-    #     elif position == "<":
-    #         direction = "left"
-    #     elif position == "V":
-    #         direction == "down"
+    @property
+    def starting_position(self):
+        for col in range(0, self.ncol):
+            for row in range(0, self.nrow):
+                if self.data[row][col] == "^":
+                    return row, col, "up"
+                
 
+    def set_deepcopy(self):
+        self.deep_copy = copy.deepcopy(self.data)
+
+
+    def move_position(self):
+        row_index, col_index, direction = self.starting_position
+
+        # also include starting position
+        self.deep_copy[row_index][col_index] = 'X'
+        
+        while direction != "end":
+
+            if direction == "up":
+                row_index, col_index, direction = self.move_up(row_index, col_index, direction)
+            elif direction == "right":
+                row_index, col_index, direction = self.move_right(row_index, col_index, direction)
+            elif direction == "down":
+                row_index, col_index, direction = self.move_down(row_index, col_index, direction)
+            elif direction == "left":
+                row_index, col_index, direction = self.move_left(row_index, col_index, direction)
+          
+        
+    def move_up(self, row_index, col_index, direction):
+        try:
+            step = self.data[row_index-1][col_index] 
+            if  step == "." or step == "^":
+                row_index = row_index-1
+                self.deep_copy[row_index][col_index] = "X"
+                direction = "up"
+            elif step == "#":
+                direction = "right"
+        except IndexError:
+            direction = "end"
+        return row_index, col_index, direction
+        
+    def move_right(self, row_index, col_index, direction):
+        try:
+            step = self.data[row_index][col_index+1] 
+            if  step == "." or step == "^":
+                col_index = col_index+1
+                self.deep_copy[row_index][col_index] = "X"
+                direction = "right"
+            elif step == "#":
+                direction = "down"
+        except IndexError:
+            direction = "end"
+        return row_index, col_index, direction
+        
+    def move_down(self, row_index, col_index, direction):
+        try:
+            step = self.data[row_index+1][col_index] 
+            if  step == "." or step == "^":
+                row_index = row_index+1
+                self.deep_copy[row_index][col_index] = "X"
+                direction = "down"
+            elif step == "#":
+                direction = "left"
+        except IndexError:
+            direction = "end"
+        return row_index, col_index, direction
+        
+    def move_left(self, row_index, col_index, direction):
+        try:
+            step = self.data[row_index][col_index-1] 
+            if  step == "." or step == "^":
+                col_index = col_index-1
+                self.deep_copy[row_index][col_index] = "X"
+                direction = "left"
+            elif step == "#":
+                direction = "up"
+        except IndexError:
+            direction = "end"
+
+        return row_index, col_index, direction
+        
+    def get_flatten(self):
+
+        flatten_list = []
+        for rows in self.deep_copy:
+            flatten_list.extend(rows)
+        
+        return flatten_list
     
+    def print_deepcopy(self):
+        for rows in self.deep_copy:
+            print(rows)
+
+
+       
+            
+
+
+        
 
 
 
@@ -29,14 +121,17 @@ def advent_of_code6a(input_str: str) -> Matrix:
     nrows = len(puzzle_input)
     ncols = len(puzzle_input[0])
     
-    puzzle_matrix = Matrix(nrow = nrows, ncol = ncols)
+    puzzle_matrix = PositionMatrix(nrow = nrows, ncol = ncols)
 
     for row in range(0, nrows):
         for col in range(0, ncols):
             puzzle_matrix[row, col] = puzzle_input[row][col]
 
+    puzzle_matrix.set_deepcopy()
+    puzzle_matrix.move_position()
+    flatten_list = puzzle_matrix.get_flatten()
     
-    return puzzle_matrix.data
+    return flatten_list.count("X")
 
 if __name__ == "__main__":
 
@@ -52,5 +147,10 @@ if __name__ == "__main__":
 ......#..."""
 
     test_output = advent_of_code6a(test_input)
-    print(test_output)
     assert test_output == 41
+
+    with open("../data/advent_of_code6/puzzle_input.txt", "r") as f:
+        puzzle_input = f.read()
+
+    puzzle_output = advent_of_code6a(puzzle_input)
+    print(f"the answer to aoc6a is: {puzzle_output}")
