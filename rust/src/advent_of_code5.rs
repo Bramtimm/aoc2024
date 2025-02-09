e01f45409d75f055dd31f8eda2dd8aaa7b561b27
bramtimm <bram.timm@gmail.com>
2025-02-09 16:56:30 +0100

    skeleton for 5a

---
+++ rust/src/advent_of_code5.rs
@@ -0,0 +1,3 @@
+pub fn advent_of_code5a(puzzle_input: &str) -> i32 {
+    0
+}
---
+++ rust/tests/advent_of_code5_integration.rs
@@ -0,0 +1,12 @@
+use advent_of_code_lib::advent_of_code5::*;
+use std::fs;
+
+
+[#test]
+fn test_advent_of_code5a() {
+
+    let file_path: &str = "../data/advent_of_code5/puzzle_input.txt";
+    let contents = fs::read_to_string(&file_path).expect("Should have been able to read the file");
+    result = advent_of_code5a(&contents);
+    assert_eq!(result, 5108)
+}
